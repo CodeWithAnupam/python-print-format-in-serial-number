7edac22e38909f7514044d1e769e652c8fc06705
CodeWithAnupam <anuamb391@gmail.com>
2022-12-19 22:00:38 +0530

    python print format in serial number

---
+++ python.py
@@ -0,0 +1,11 @@
+a = ['', 'anupam', 'sutapa', 'riya', 'sanju', 'rosni', 'sonali', 'susmita', 'rahul', 'mohasin', 'moha', 'jonaki', 'juliet', 'raaj','priya', 'harimahan', ' haridas', 'haripal', 'suman', 'joya', 'prantik', 'kartik', 'nayra', 'arif', 'souvik', 'ripon', 'puspa', 'allu arjun', 'ram', 'xukai', 'sunni', 'nobita', 'sunio', 'sinchain', 'gyan', 'arman', 'oggy', 'jack', 'paplu', 'dhaplu', 'jhaplu', 'sizuka', 'priti', 'ruhi', 'kenichi', 'ninja hattori', 'sisumanu', 'shinzo', 'bob', 28376, '']
+
+
+
+
+for i,each in enumerate(a,start=1): 
+    print("{} => {}".format(i,each))
+
+
+
+
